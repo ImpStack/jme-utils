6a93680f1d45c836c99a3d40233f5a4650d0f848
rvandoosselaer <remy@impstack.org>
2018-04-04 13:23:59 +0200

    set samples and add logging

--- src/main/java/org/impstack/jme/state/SceneProcessorState.java
+++ src/main/java/org/impstack/jme/state/SceneProcessorState.java
@@ -3,19 +3,26 @@ package org.impstack.jme.state;
 import com.jme3.app.Application;
 import com.jme3.app.state.BaseAppState;
 import com.jme3.post.FilterPostProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A state that maintains a FilterPostProcessor state on the main viewport ({@link Application#getViewPort()}
+ * Only one {@link FilterPostProcessor} should be set on a {@link com.jme3.renderer.ViewPort}.
  */
 public class SceneProcessorState extends BaseAppState {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SceneProcessorState.class);
 
     private FilterPostProcessor fpp;
 
     @Override
     protected void initialize(Application app) {
         fpp = new FilterPostProcessor(app.getAssetManager());
+        fpp.setNumSamples(app.getContext().getSettings().getSamples());
 
         app.getViewPort().addProcessor(fpp);
+        LOG.debug("Adding {} with {} samples on {}", fpp, fpp.getNumSamples(), app.getViewPort());
     }
 
     @Override
@@ -29,6 +36,7 @@ public class SceneProcessorState extends BaseAppState {
     @Override
     protected void cleanup(Application app) {
         app.getViewPort().removeProcessor(fpp);
+        LOG.debug("Removing {} from {}", fpp, app.getViewPort());
     }
 
     public FilterPostProcessor getFpp() {
