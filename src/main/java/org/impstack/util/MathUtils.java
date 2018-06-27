8fa5f4cf181885bc83d30112b607ca94b292b2a8
rvandoosselaer <remy@impstack.org>
2018-06-27 13:48:31 +0200

    html safe javadoc

--- src/main/java/org/impstack/util/MathUtils.java
+++ src/main/java/org/impstack/util/MathUtils.java
@@ -11,7 +11,7 @@ public final class MathUtils {
      * @param min the minimum value
      * @param max the maximum value
      * @param value the value
-     * @return min if the value < min, max if the value > max, or the value
+     * @return min if the value &lt; min, max if the value &gt; max, or the value
      */
     public static float range(float min, float max, float value) {
         return Math.min(Math.max(min, value), max);
