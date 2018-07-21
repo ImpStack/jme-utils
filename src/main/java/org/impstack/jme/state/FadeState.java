5cd3da97caea5ab4d46b4129b65c248ddc0feb4b
Remy Van Doosselaer <remy@impstack.org>
2018-07-21 20:45:11 +0200

    A FadeState that can hide and show the scene using an overlay.

---
+++ src/main/java/org/impstack/jme/state/FadeState.java
@@ -0,0 +1,100 @@
+package org.impstack.jme.state;
+
+import com.jme3.app.Application;
+import com.jme3.app.state.BaseAppState;
+import com.jme3.math.ColorRGBA;
+import com.simsilica.lemur.Panel;
+import org.impstack.jme.ApplicationContext;
+import org.impstack.jme.lemur.GuiHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * An application state that performs a fade in (color to scene) and fade out (scene to color).
+ * The duration of the fade in/out animation can be set using {@link #setDuration(float)}.
+ */
+public class FadeState extends BaseAppState {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FadeState.class);
+
+    private Panel overlay;
+    private ColorRGBA overlayColor = ColorRGBA.Black;
+    private boolean attachOverlay = true;
+    private boolean animationRunning = false;
+    private float duration = 0.3f;
+    private float value = 0f; // 0 = transparent; 1 = black
+    private float direction = 1f; // 1 = fade out; -1 = fade in
+
+    public FadeState() {
+    }
+
+    public FadeState(ColorRGBA overlayColor, boolean attachOverlay) {
+        this.overlayColor = overlayColor;
+        this.attachOverlay = attachOverlay;
+    }
+
+    @Override
+    protected void initialize(Application app) {
+        overlay = new Panel(GuiHelper.getWidth(), GuiHelper.getHeight(), overlayColor);
+        overlay.setLocalTranslation(0, GuiHelper.getHeight(), 999);
+        value = attachOverlay ? 1 : 0;
+        overlay.setAlpha(value);
+        ApplicationContext.INSTANCE.getGuiNode().attachChild(overlay);
+    }
+
+    @Override
+    protected void cleanup(Application app) {
+        overlay.removeFromParent();
+    }
+
+    @Override
+    protected void onEnable() {
+    }
+
+    @Override
+    protected void onDisable() {
+    }
+
+    @Override
+    public void update(float tpf) {
+        if (animationRunning) {
+            value += tpf * (direction / duration);
+
+            if (direction == -1 && value < 0) {
+                value = 0;
+                animationRunning = false;
+                LOG.trace("Fade in completed");
+            }
+
+            if (direction == 1 && value > 1) {
+                value = 1;
+                animationRunning = false;
+                LOG.trace("Fade out completed");
+            }
+
+            overlay.setAlpha(value);
+        }
+    }
+
+    public void fadeIn() {
+        value = 1;
+        direction = -1;
+        animationRunning = true;
+        LOG.trace("Fading in...");
+    }
+
+    public void fadeOut() {
+        value = 0;
+        direction = 1;
+        animationRunning = true;
+        LOG.trace("Fading out...");
+    }
+
+    public float getDuration() {
+        return duration;
+    }
+
+    public void setDuration(float duration) {
+        this.duration = duration;
+    }
+}
---
+++ src/test/java/org/impstack/jme/state/FadeTest.java
@@ -0,0 +1,40 @@
+package org.impstack.jme.state;
+
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.shape.Sphere;
+import org.impstack.jme.JmeLauncher;
+import org.impstack.jme.scene.GeometryUtils;
+
+/**
+ * A test case for the {@link FadeState}
+ * Press 'i' to trigger a fade in, press 'o' to trigger a fade out.
+ */
+public class FadeTest extends JmeLauncher implements ActionListener {
+
+    public static void main(String[] args) {
+        new FadeTest().start();
+    }
+
+    @Override
+    public void init() {
+        stateManager.attach(new FadeState(ColorRGBA.Green, true));
+
+        rootNode.attachChild(new GeometryUtils(this).createGeometry(new Sphere(32, 32, 1), ColorRGBA.Blue));
+
+        inputManager.addMapping("fadeIn", new KeyTrigger(KeyInput.KEY_I));
+        inputManager.addMapping("fadeOut", new KeyTrigger(KeyInput.KEY_O));
+        inputManager.addListener(this, "fadeIn", "fadeOut");
+    }
+
+    @Override
+    public void onAction(String name, boolean isPressed, float tpf) {
+        if ("fadeIn".equals(name) && !isPressed) {
+            stateManager.getState(FadeState.class).fadeIn();
+        } else if ("fadeOut".equals(name) && !isPressed) {
+            stateManager.getState(FadeState.class).fadeOut();
+        }
+    }
+}
