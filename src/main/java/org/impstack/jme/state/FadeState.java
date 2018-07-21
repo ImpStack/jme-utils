75c33f10a432e26370f124091abf79dec8febbbf
Remy Van Doosselaer <remy@impstack.org>
2018-07-21 20:56:59 +0200

    expose the animation running boolean

--- src/main/java/org/impstack/jme/state/FadeState.java
+++ src/main/java/org/impstack/jme/state/FadeState.java
@@ -97,4 +97,8 @@ public class FadeState extends BaseAppState {
     public void setDuration(float duration) {
         this.duration = duration;
     }
+
+    public boolean isAnimationRunning() {
+        return animationRunning;
+    }
 }
