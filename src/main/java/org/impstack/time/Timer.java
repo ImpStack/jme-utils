4850fa3295a36404b81927f686c46bb8a515615a
rvandoosselaer <remy@impstack.org>
2018-09-11 13:36:04 +0200

    change the timer to nanoseconds accuracy

--- src/main/java/org/impstack/time/Timer.java
+++ src/main/java/org/impstack/time/Timer.java
@@ -1,24 +1,39 @@
 package org.impstack.time;
 
+import java.util.concurrent.TimeUnit;
+
 /**
- * A simple timer class, with a milliseconds accuracy.
+ * A simple timer class, with a nanoseconds accuracy.
  * This timer is NOT threadsafe!
  */
 public final class Timer {
 
+    private static final long NANO_2_MILLIS = 1000000L;
+
     private long start;
 
     public Timer() {
-        start = System.currentTimeMillis();
+        start = System.nanoTime();
     }
 
+    /**
+     * @return the elapsed time in milliseconds
+     */
     public long read() {
-        return System.currentTimeMillis() - start;
+        return getNanoTime() / NANO_2_MILLIS;
+    }
+
+    public long read(TimeUnit timeUnit) {
+        return timeUnit.convert(getNanoTime(), TimeUnit.NANOSECONDS);
     }
 
     public long reset() {
         long time = read();
-        start = System.currentTimeMillis();
+        start = System.nanoTime();
         return time;
+    }
+
+    private long getNanoTime() {
+        return System.nanoTime() - start;
     }
 }
