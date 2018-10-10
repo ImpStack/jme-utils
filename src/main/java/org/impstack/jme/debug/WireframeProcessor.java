d2bd4641c2b63e77519a5551e0f1f5d555863f5d
rvandoosselaer <remy@impstack.org>
2018-10-10 12:40:10 +0200

    A wireframe processor to render a wireframe on an entire viewport

---
+++ src/main/java/org/impstack/jme/debug/WireframeProcessor.java
@@ -0,0 +1,82 @@
+package org.impstack.jme.debug;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.post.SceneProcessor;
+import com.jme3.profile.AppProfiler;
+import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.ViewPort;
+import com.jme3.renderer.queue.RenderQueue;
+import com.jme3.texture.FrameBuffer;
+
+/**
+ * A {@link SceneProcessor} that forces a wireframe material on all the objects in the scene.
+ * The color of the wireframe can be set using the {@link #setColor(ColorRGBA)} method.
+ * The width of the lines of the wireframe can be set using the {@link #setLineWidth(float)} method.
+ */
+public class WireframeProcessor implements SceneProcessor {
+
+    private static final String WIREFRAME_MATERIAL = "Materials/Wireframe.j3m";
+
+    private Material wireframeMaterial;
+    private RenderManager renderManager;
+
+    public WireframeProcessor(AssetManager assetManager) {
+        wireframeMaterial = assetManager.loadMaterial(WIREFRAME_MATERIAL);
+    }
+
+    @Override
+    public void initialize(RenderManager rm, ViewPort vp) {
+        renderManager = rm;
+    }
+
+    @Override
+    public void reshape(ViewPort vp, int w, int h) {
+    }
+
+    @Override
+    public boolean isInitialized() {
+        return renderManager != null;
+    }
+
+    @Override
+    public void preFrame(float tpf) {
+    }
+
+    @Override
+    public void postQueue(RenderQueue rq) {
+        renderManager.setForcedMaterial(wireframeMaterial);
+    }
+
+    @Override
+    public void postFrame(FrameBuffer out) {
+        renderManager.setForcedMaterial(null);
+    }
+
+    @Override
+    public void cleanup() {
+        renderManager.setForcedMaterial(null);
+    }
+
+    @Override
+    public void setProfiler(AppProfiler profiler) {
+    }
+
+    /**
+     * Set a custom wireframe color. The default color is (RGBA: 0.2 0.4 1.0 1.0)
+     * @param color the color value
+     */
+    public void setColor(ColorRGBA color) {
+        wireframeMaterial.setColor("Color", color);
+    }
+
+    /**
+     * Set a custom line width. The default line width is 1.0f
+     * @param width the width of the line
+     */
+    public void setLineWidth(float width) {
+        wireframeMaterial.getAdditionalRenderState().setLineWidth(width);
+    }
+
+}
---
+++ src/test/java/org/impstack/jme/debug/WireframeProcessorTest.java
@@ -0,0 +1,63 @@
+package org.impstack.jme.debug;
+
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.Trigger;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Sphere;
+import org.impstack.jme.JmeLauncher;
+import org.impstack.jme.scene.GeometryUtils;
+
+/**
+ * A test case for the {@link WireframeProcessor}.
+ * When you press 'P' a wireframe is displayed on the entire scene with a random color.
+ */
+public class WireframeProcessorTest extends JmeLauncher implements ActionListener {
+
+    private static final String MAPPING = "toggleWireframe";
+    private static final Trigger TRIGGER = new KeyTrigger(KeyInput.KEY_P);
+
+    private WireframeProcessor wireframeProcessor;
+
+    public static void main(String[] args) {
+        new WireframeProcessorTest().start();
+    }
+
+    @Override
+    public void init() {
+        GeometryUtils geometryUtils = new GeometryUtils(this);
+
+        Geometry box = geometryUtils.createGeometry(new Box(0.5f, 0.5f, 0.5f), ColorRGBA.Green);
+        Geometry sphere = geometryUtils.createGeometry(new Sphere(32, 32, 0.5f), ColorRGBA.Red);
+
+        box.move(-2f, 0, 0);
+        sphere.move(2f, 0, 0);
+
+        rootNode.attachChild(box);
+        rootNode.attachChild(sphere);
+
+        wireframeProcessor = new WireframeProcessor(assetManager);
+
+        inputManager.addMapping(MAPPING, TRIGGER);
+        inputManager.addListener(this, MAPPING);
+    }
+
+    @Override
+    public void onAction(String name, boolean isPressed, float tpf) {
+        if (name.equals(MAPPING) && !isPressed) {
+            if (viewPort.getProcessors().contains(wireframeProcessor)) {
+                viewPort.removeProcessor(wireframeProcessor);
+            } else {
+                // set a custom color
+                wireframeProcessor.setColor(ColorRGBA.randomColor());
+                // set a custom line thickness
+                wireframeProcessor.setLineWidth(FastMath.nextRandomInt(1, 5));
+                viewPort.addProcessor(wireframeProcessor);
+            }
+        }
+    }
+}
